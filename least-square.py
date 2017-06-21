9beafce77a9327690146b28b23f7519582a64655
esoroush <e.soroush@hotmail.com>
2017-06-21 16:10:07 +0430

    adds max epoch for not converging conditions

--- least-square.py
+++ least-square.py
@@ -19,10 +19,15 @@ def constrained_least_square():
     l = np.random.rand(1)
     x = np.dot(np.linalg.inv(np.dot(A.T, A) + 2*l*np.eye(A.shape[0])), np.dot(A.T, b))
     grad_l = np.dot(x.T, x) - 1
+    epoch = 0
+    max_epoch = 500
     while np.linalg.norm(grad_l) > delta:
-        l += epsilon*grad_l.squeeze()
+        l += epsilon*grad_l.squeeze() # gradient ascent
         x = np.dot(np.linalg.inv(np.dot(A.T, A) + 2*l*np.eye(A.shape[0])), np.dot(A.T, b))
         grad_l = np.dot(x.T, x) - 1
+        epoch += 1
+        if epoch > max_epoch:
+            print("Couldn't converge in %d epochs"%max_epoch)
     print("x_hat: {} and norm(x) is: {}".format(x, np.linalg.norm(x)))
     print("error is: %.4f"%np.linalg.norm(np.dot(A, x)-b) )
     
@@ -31,7 +36,7 @@ def least_square():
     x_hat = np.random.rand(5, 1)
     grad = np.dot(np.dot(A.T, A), x_hat) - np.dot(A.T, b)
     while np.linalg.norm(grad) > delta:
-        x_hat -= epsilon*grad
+        x_hat -= epsilon*grad # gradient descent
         grad = np.dot(np.dot(A.T, A), x_hat) - np.dot(A.T, b)
     print("x_hat is {}".format(x_hat))
     print("error is: %.4f"%(np.linalg.norm(np.dot(A, x_hat)-b)))
